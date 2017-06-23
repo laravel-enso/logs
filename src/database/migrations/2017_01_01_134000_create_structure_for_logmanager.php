44d5f285fae9fb8872c20e15ccd1d70d5d4e199c
mishu <mishu@xtelecom.ro>
2017-06-23 14:15:19 +0300

    updated structure migration

--- src/database/migrations/2017_01_01_134000_create_structure_for_logmanager.php
+++ src/database/migrations/2017_01_01_134000_create_structure_for_logmanager.php
@@ -5,18 +5,18 @@ use LaravelEnso\Core\app\Classes\StructureManager\StructureMigration;
 class CreateStructureForLogManager extends StructureMigration
 {
     protected $permissionGroup = [
-        'name' => 'system.logs', 'description' => 'Logs Permissions Group',
+        'name' => 'system.logs', 'description' => 'Logs permissions group',
     ];
 
     protected $permissions = [
         ['name' => 'system.logs.index', 'description' => 'Logs index', 'type' => 0, 'default' => false],
-        ['name' => 'system.logs.show', 'description' => 'Show Log', 'type' => 0, 'default' => false],
-        ['name' => 'system.logs.download', 'description' => 'Download Log', 'type' => 0, 'default' => false],
-        ['name' => 'system.logs.destroy', 'description' => 'Delete Log', 'type' => 1, 'default' => false],
+        ['name' => 'system.logs.show', 'description' => 'Show log', 'type' => 0, 'default' => false],
+        ['name' => 'system.logs.download', 'description' => 'Download log', 'type' => 0, 'default' => false],
+        ['name' => 'system.logs.destroy', 'description' => 'Delete log', 'type' => 1, 'default' => false],
     ];
 
     protected $menu = [
-        'name' => 'Logs', 'icon' => 'fa fa-fw fa-terminal', 'link' => 'system/logs', 'has_children' => 0,
+        'name' => 'Logs', 'icon' => 'fa fa-fw fa-terminal', 'link' => 'system/logs', 'has_children' => false,
     ];
 
     protected $parentMenu = 'System';
