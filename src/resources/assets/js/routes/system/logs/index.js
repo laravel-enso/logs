9ca53562338c31cbda1efc6084c129c72f6edfd4
mishu <mishu@xtelecom.ro>
2017-10-01 23:21:15 +0300

    remove duplicate files

--- src/resources/assets/js/routes/system/logs/index.js
+++
@@ -1,9 +0,0 @@
-export const Index = {
-    name: 'system.logs.index',
-    path: '',
-    component: require('../../../pages/system/logs/Index.vue'),
-    meta: {
-        breadcrumb: 'index',
-        title: 'Logs Index'
-    },
-};
--- src/resources/assets/js/routes/system/logs/show.js
+++
@@ -1,9 +0,0 @@
-export const Show = {
-    name: 'system.logs.show',
-    path: ':log',
-    component: require('../../../pages/system/logs/Show.vue'),
-    meta: {
-        breadcrumb: 'show',
-        title: 'Show Log'
-    }
-};
