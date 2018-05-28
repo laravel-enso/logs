015be4f7aec02b9ab0951176f64a834dd7bd03b9
mishu <mishu@xtelecom.ro>
2018-05-28 13:25:37 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_134000_create_structure_for_logmanager.php
+++ src/database/migrations/2017_01_01_134000_create_structure_for_logmanager.php
@@ -9,14 +9,14 @@ class CreateStructureForLogManager extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'system.logs.index', 'description' => 'Logs index', 'type' => 0, 'default' => false],
-        ['name' => 'system.logs.show', 'description' => 'Show log', 'type' => 0, 'default' => false],
-        ['name' => 'system.logs.download', 'description' => 'Download log', 'type' => 0, 'default' => false],
-        ['name' => 'system.logs.destroy', 'description' => 'Delete log', 'type' => 1, 'default' => false],
+        ['name' => 'system.logs.index', 'description' => 'Logs index', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.logs.show', 'description' => 'Show log', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.logs.download', 'description' => 'Download log', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.logs.destroy', 'description' => 'Delete log', 'type' => 1, 'is_default' => false],
     ];
 
     protected $menu = [
-        'name' => 'Logs', 'icon' => 'terminal', 'link' => 'system.logs.index', 'order' => 999, 'has_children' => false,
+        'name' => 'Logs', 'icon' => 'terminal', 'link' => 'system.logs.index', 'order_index' => 999, 'has_children' => false,
     ];
 
     protected $parentMenu = 'System';
