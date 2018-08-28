d8a1a01ad869611e4577eb6c500b5a7e8de97db5
mishu <mishu@xtelecom.ro>
2018-08-28 11:36:02 +0300

    updates composer.json

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -8,17 +8,19 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadDependencies();
-        $this->publishesAll();
+        $this->load();
+
+        $this->publish();
     }
 
-    private function loadDependencies()
+    private function load()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
-    private function publishesAll()
+    private function publish()
     {
         $this->publishes([
             __DIR__.'/resources/assets/js' => resource_path('assets/js'),
