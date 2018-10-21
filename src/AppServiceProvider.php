3deaaeb4e8dc6cf5eee08b7b6946e35d62e47210
mishu <mishu@xtelecom.ro>
2018-10-21 14:42:51 +0300

    updates code style

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -8,19 +8,20 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->load();
-
-        $this->publish();
+        $this->loadDependencies()
+            ->publishDependencies();
     }
 
-    private function load()
+    private function loadDependencies()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/api.php');
 
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        return $this;
     }
 
-    private function publish()
+    private function publishDependencies()
     {
         $this->publishes([
             __DIR__.'/resources/js' => resource_path('js'),
