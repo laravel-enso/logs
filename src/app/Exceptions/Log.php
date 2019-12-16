15ef932f7de105f96f0f3f35efe3e78b7056c1af
Adrian Ocneanu <aocneanu@gmail.com>
2019-12-16 13:55:32 +0200

    Update Log.php

--- src/app/Exceptions/Log.php
+++ src/app/Exceptions/Log.php
@@ -6,7 +6,7 @@ use LaravelEnso\Helpers\app\Exceptions\EnsoException;
 
 class Log extends EnsoException
 {
-    public static function overLimit($limit)
+    public static function size($limit)
     {
         return new self(__('Log file exceeds the limit of :limit MB', ['limit' => $limit]));
     }
