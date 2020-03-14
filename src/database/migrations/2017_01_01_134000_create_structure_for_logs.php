a50c538bd3b71b4da63dd43acaf59b62dc1f7f78
mishu <mishu@xtelecom.ro>
2020-03-14 18:32:05 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_134000_create_structure_for_logs.php
+++ src/database/migrations/2017_01_01_134000_create_structure_for_logs.php
@@ -1,15 +1,14 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForLogs extends Migration
 {
     protected $permissions = [
-        ['name' => 'system.logs.index', 'description' => 'Logs index', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.logs.show', 'description' => 'Show log', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.logs.download', 'description' => 'Download log', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.logs.destroy', 'description' => 'Delete log', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'system.logs.index', 'description' => 'Logs index', 'is_default' => false],
+        ['name' => 'system.logs.show', 'description' => 'Show log', 'is_default' => false],
+        ['name' => 'system.logs.download', 'description' => 'Download log', 'is_default' => false],
+        ['name' => 'system.logs.destroy', 'description' => 'Delete log', 'is_default' => false],
     ];
 
     protected $menu = [
