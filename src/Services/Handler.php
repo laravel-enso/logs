5dcc64aed08448fa999b281fd91131178f23284f
mishu <mishu@xtelecom.ro>
2021-10-11 10:05:28 +0300

    fixes tests

--- src/Services/Handler.php
+++ src/Services/Handler.php
@@ -5,6 +5,7 @@ namespace LaravelEnso\Logs\Services;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use LaravelEnso\Helpers\Services\Decimals;
 
 abstract class Handler
@@ -14,11 +15,13 @@ abstract class Handler
     protected function log($file): array
     {
         $size = $this->formattedSize(File::size($file));
+        $name = Str::afterLast($file, DIRECTORY_SEPARATOR);
+        $extension = Str::afterLast($name, '.');
 
         return [
             'name' => Collection::wrap(explode(DIRECTORY_SEPARATOR, $file))->last(),
             'size' => $size,
-            'visible' => $size <= self::LogSizeLimit && $file->getExtension() === 'log',
+            'visible' => $size <= self::LogSizeLimit && $extension === 'log',
             'modified' => Carbon::createFromTimestamp(File::lastModified($file)),
         ];
     }
