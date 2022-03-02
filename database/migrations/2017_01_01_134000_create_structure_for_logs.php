17518355a86b8fde51b8ead2d956de0706637a7f
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 14:17:19 +0200

    updated migration

--- database/migrations/2017_01_01_134000_create_structure_for_logs.php
+++ database/migrations/2017_01_01_134000_create_structure_for_logs.php
@@ -2,7 +2,7 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForLogs extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'system.logs.index', 'description' => 'Logs index', 'is_default' => false],
@@ -16,4 +16,4 @@ class CreateStructureForLogs extends Migration
     ];
 
     protected ?string $parentMenu = 'System';
-}
+};
